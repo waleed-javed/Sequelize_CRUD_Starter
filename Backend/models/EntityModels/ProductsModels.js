2acbeb033601f3f6ddac7d2d63d3b0ea6082fcec
waleed javed <waleed.javed@gmail.com>
2022-09-12 22:13:02 +0500

    made minor changes to model to work with associations, might revert based on need

--- Backend/models/EntityModels/ProductsModels.js
+++ Backend/models/EntityModels/ProductsModels.js
@@ -1,19 +1,60 @@
-module.exports = (sequelize, DataTypes) =>{
-    const Product = sequelize.define("products",
-    //model meta deta
-    {
-        id:{
-            type:DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull:false,
-        },
-        title:{
-            type: DataTypes.STRING,
-            allowNull: false,       
-        },
-        price:DataTypes.INTEGER,
-        description: DataTypes.TEXT,
-    })
+const {Model} = require('sequelize');
 
-    return Product;
-}
+module.exports =(sequelize, DataTypes) =>{
+
+    // creating class model to defie associationa
+    // this model will also elaborate the cardinality
+    class Product extends Model {
+
+        static associate(models){
+            this.hasMany(models.Review,{ foreignKey:'review_id' });
+        }
+    }
+
+    //intiaiing class model 
+    Product.init({
+            Id:{
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull:false,
+                primaryKey:true
+            },
+            product_title:{
+                type: DataTypes.STRING,
+            },
+            product_price:{
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },
+        {
+            sequelize,
+            modelName:'Product',
+            tableName:'Product',            
+        }
+    );
+
+    return Product; //exported model defintion
+}; // class ends
+
+
+
+// module.exports = (sequelize, DataTypes) =>{
+//     const Product = sequelize.define("products",
+//     //model meta deta
+//     {
+//         id:{
+//             type:DataTypes.UUID,
+//             defaultValue: DataTypes.UUIDV4,
+//             allowNull:false,
+//         },
+//         title:{
+//             type: DataTypes.STRING,
+//             allowNull: false,       
+//         },
+//         price:DataTypes.INTEGER,
+//         description: DataTypes.TEXT,
+//     })
+
+//     return Product;
+// }
--- Backend/models/EntityModels/ReviewsModel.js
+++ Backend/models/EntityModels/ReviewsModel.js
@@ -1,14 +1,68 @@
-module.exports = (sequelize, DataTypes) =>{
-    const Review = sequelize.define("products",
-    //model meta deta
-    {
-        id:{
-            type:DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-        },
-        rating:DataTypes.INTEGER,
-        description: DataTypes.TEXT,
-    })
+const {Model} = require('sequelize');
 
-    return Review;
-}
+module.exports =(sequelize, DataTypes) =>{
+
+    /* **** Model Definition Parameters ***
+    * There can be many reviews against one product and 
+    * each review will have following:
+    * A unique Id
+    * A Integer Rating ranging between 0 and 5
+    * and a secondary key 
+    */
+
+    // creating class model to defie associationa
+    // this model will also elaborate the cardinality
+    class Review extends Model {
+
+        static associate(models){
+            this.belongsTo(models.Product,{ foreignKey:'Id' });
+        }
+    }
+
+    //intiaiing class model 
+    Review.init({
+            id:{
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey:true,
+                allowNull:false
+            },
+            review_id:{
+                type: DataTypes.UUID,
+            },
+            rating:{
+                type: DataTypes.INTEGER
+            }
+        },
+        // Extra tbale options for model development 
+        {
+            sequelize,
+            modelName: 'Review',
+            tableName:'Review'
+        }
+    );// review model ends
+
+    return Review; //exported model defintion
+}; // class ends
+
+
+
+
+
+
+
+
+// module.exports = (sequelize, DataTypes) =>{
+//     const Review = sequelize.define("products",
+//     //model meta deta
+//     {
+//         review_id:{
+//             type:DataTypes.UUID,
+//             defaultValue: DataTypes.UUIDV4,
+//         },
+//         rating:DataTypes.INTEGER,
+//         description: DataTypes.TEXT,
+//     })
+
+//     return Review;
+// }
